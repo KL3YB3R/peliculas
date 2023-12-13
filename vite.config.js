90b120e8affd3cf9efe3d7f1febf0cd54c706c43
Kleyber Infante <42837223+KL3YB3R@users.noreply.github.com>
2023-12-12 22:07:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import fs from 'fs'; 
-const host = 'heroes.test';
 
 export default defineConfig({
     server: {
-        host,
-        hmr: { host },
-        https: {
-            key: fs.readFileSync(`/path/to/${host}.key`),
-            cert: fs.readFileSync(`/path/to/${host}.crt`),
+        hmr: {
+            host: "localhost",
         },
     },
     plugins: [
