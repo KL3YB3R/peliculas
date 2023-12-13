c3c35b2fd660b1520563b63a594f3a82e434ceec
Kleyber Infante <42837223+KL3YB3R@users.noreply.github.com>
2023-12-13 12:25:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/',
-    server: {
-        hmr: {
-            host: "localhost",
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
